76cb282c276b52f509cc31bf4a55067c5dc04729
PremJibon <126474324+PremJibon@users.noreply.github.com>
2023-07-11 13:12:20 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TicTacToe from './assets/tic-tac-toe/tic-tac-toe'
+
+function App() {
+  
+
+  return (
+    <>
+      <TicTacToe />
+    </>
+  )
+}
+
+export default App
